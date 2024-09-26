5c9b48c3af387627e86f54d245de1df5891b269a
Atvr0 <artvro12345@gmail.com>
2024-09-26 20:58:05 +0300

    zxv

--- app/data/files_actions.py
+++ app/data/files_actions.py
@@ -2,7 +2,7 @@ import json
 
 from app.data import list_files
 
-def open_file(path: str = list_files. ANIMALS) -> list:
+def open_file(path: str = list_files.ANIMALS) -> list:
     with open(path, "r", encoding="utf=8") as file:
         animals = json.load(file)
 
