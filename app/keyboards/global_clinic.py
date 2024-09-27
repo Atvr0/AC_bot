3be186b80cf3d2f5047bf4a871d2a00f4299f73d
Atvr0 <artvro12345@gmail.com>
2024-09-27 19:16:20 +0300

    d

--- app/keyboards/global_clinic.py
+++ app/keyboards/global_clinic.py
@@ -4,6 +4,7 @@ def global_clining_keyboard_bulder():
     builder = ReplyKeyboardBuilder()
     builder.button(text="Показати тварин")
     builder.button(text="Додати новy Тварина")
+    builder.button(text="Список вилікуваних тварин")
     builder.button(text="Показати всі відгуки")
     builder.button(text="Додати новий відгук")
     builder.adjust(1)
